36f8ee28ade91dfdbcd0ac307382868cbb6f08e2
Spencer Norris <Spencer.serv@gmail.com>
2023-08-29 15:52:46 -0500

    Added Steve

--- src/App.js
+++ src/App.js
@@ -1,43 +1,62 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { getAllJokes } from "./services/jokeService";
+import { getAllJokes, submitJoke } from "./services/jokeService";
+import stevePic from "./assets/steve.png"
 
 export const App = () => {
-  const [handleInputEvent, setOnChangeInputEvent] = useState([]);
-  const [allJokes, setAllJokes] = useState([])
+  const [inputEvent, setInputEvent] = useState("");
+
+  const [allJokes, setAllJokes] = useState([]);
 
   useEffect(() => {
     getAllJokes().then((jokesArray) => {
-      setAllJokes(jokesArray)
-      console.log(jokesArray)
-    })
-  }, [])
+      setAllJokes(jokesArray);
+      console.log(jokesArray);
+    });
+  }, [inputEvent]);
 
-  const handleButtonClick = () => {
-    console.log(handleInputEvent)
+  //   const handleButtonClick = () => {
+  //     console.log(inputEvent)
+  //   }
+
+  const click = () => {
+    submitJoke(inputEvent)
+    setInputEvent('');
   }
-
+    
 
   return (
-    <div>
-      <header className="app-heading">
+    <div className="app-container">
+      <header className="app-heading"><div className="app-heading-circle">
+   <img className="app-logo" src={stevePic} alt="Good job Steve" />
+ </div>
         <h1 className="app-heading-text">Chuckles Checklist</h1>
       </header>
 
       <h2>Add Joke</h2>
+      <div className="joke-add-form">
       <input
-        className=""
+        className="joke-input"
         type="text"
-        value={handleInputEvent}
+        value={inputEvent}
         placeholder="New One Liner"
-        onChange={(handleInputEvent) => {
-          setOnChangeInputEvent(handleInputEvent.target.value);
-          
+        onChange={(Event) => {
+          setInputEvent(Event.target.value);
         }}
       />
-      <button type="button" onClick={handleButtonClick}>Submit Joke</button><br></br>
-      <div style={{fontSize: 24 }}>You Entered: {handleInputEvent}</div>
+      <button className="joke-input-submit" onClick={inputEvent ? click : null}>
+        Submit Joke
+      </button></div>
+      <br></br>
+      <div>
+        {allJokes.map((joke) => {
+          return (
+            <div key={joke.id}>
+              <div>{joke.text}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-
   );
 };
--- src/services/jokeService.js
+++ src/services/jokeService.js
@@ -5,3 +5,28 @@ export const getAllJokes = () => {
 export const saveJokeInJokeObject = () => {
     fetch (`http://localhost:8088/jokes`)
 }
+
+export const submitJoke = async (inputEvent) => {
+    const jokeObject = {
+      text: inputEvent,
+      told: false,
+    };
+    try {
+    const response = await fetch("http://localhost:8088/jokes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jokeObject),
+    });
+    if (response.ok) {
+      console.log('Joke submitted successfully');
+      // You can reset the userInput state here if needed
+    
+    } else {
+      console.error('Failed to submit joke');
+    }
+  } catch (error) {
+    console.error('Error submitting joke:', error);
+  }
+};
