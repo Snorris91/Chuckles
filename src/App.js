adb8fe05b9476160f1018dddb06be85802278ae0
Spencer Norris <Spencer.serv@gmail.com>
2023-08-30 16:01:06 -0500

    WIP continue button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { getAllJokes, submitJoke } from "./services/jokeService";
-import stevePic from "./assets/steve.png"
+import { changeJokeValue, getAllJokes, submitJoke } from "./services/jokeService";
+import stevePic from "./assets/steve.png";
 
 export const App = () => {
   const [inputEvent, setInputEvent] = useState("");
-
   const [allJokes, setAllJokes] = useState([]);
+  const [toldJokes, setToldJokes] = useState([]);
+  const [notToldJokes, setNotToldJokes] = useState([]);
+ // const [jokeValueChange, setJokeValueChange] = useState(false)
 
   useEffect(() => {
     getAllJokes().then((jokesArray) => {
@@ -15,40 +17,101 @@ export const App = () => {
     });
   }, [inputEvent]);
 
-  //   const handleButtonClick = () => {
-  //     console.log(inputEvent)
-  //   }
+  useEffect(() => {
+    const checkJoke = (joke) => {
+      return joke.told === true;
+    };
+    const jokes = allJokes.filter(checkJoke);
 
-  const click = () => {
-    submitJoke(inputEvent)
-    setInputEvent('');
-  }
-    
+    setToldJokes(jokes);
+    //console.log(jokes);
+  }, [allJokes]);
+
+  useEffect(() => {
+    const checkJoke = (joke) => {
+      return joke.told === false;
+    };
+    const jokes = allJokes.filter(checkJoke);
+
+    setNotToldJokes(jokes);
+   // console.log(jokes);
+  }, [allJokes]);
+
+  const clickSubmit = () => {
+    submitJoke(inputEvent);
+    setInputEvent("");
+  };
+
+// const jokeChange =  (joke) => { 
+//       changeJokeValue(joke)
+
+//     }
+  
+
+
 
   return (
+    <>
     <div className="app-container">
-      <header className="app-heading"><div className="app-heading-circle">
-   <img className="app-logo" src={stevePic} alt="Good job Steve" />
- </div>
+      <header className="app-heading">
+        <div className="app-heading-circle">
+          <img className="app-logo" src={stevePic} alt="Good job Steve" />
+        </div>
         <h1 className="app-heading-text">Chuckles Checklist</h1>
       </header>
 
       <h2>Add Joke</h2>
       <div className="joke-add-form">
-      <input
-        className="joke-input"
-        type="text"
-        value={inputEvent}
-        placeholder="New One Liner"
-        onChange={(Event) => {
-          setInputEvent(Event.target.value);
-        }}
-      />
-      <button className="joke-input-submit" onClick={inputEvent ? click : null}>
-        Submit Joke
-      </button></div>
+        <input
+          className="joke-input"
+          type="text"
+          value={inputEvent}
+          placeholder="New One Liner"
+          onChange={(Event) => {
+            setInputEvent(Event.target.value);
+          }}
+        />
+        <button
+          className="joke-input-submit"
+          onClick={inputEvent ? clickSubmit : null}
+        >
+          Submit Joke
+        </button>
+      </div>
+
+      <div >
+        <section className="joke-lists-container">
+        <article className="joke-list-container">
+          <h2>Not Told Jokes<span className="told-count">{notToldJokes.length}</span></h2>
+          {notToldJokes.map((joke) => {
+            return (
+              <section className="joke-list-item" key={joke.id}>
+                <div>{joke.text}
+                <span>
+                  <button onClick={() => changeJokeValue(joke)}>SAD BUTTON</button>
+                </span>
+                </div>
+              </section>
+            );
+          })}
+        </article>
+        
+        <article className="joke-list-container">
+          <h2>Told Jokes!<span className="untold-count">{toldJokes.length}</span></h2>
+          {toldJokes.map((joke) => {
+            return (
+              <section key={joke.id}>
+                <div className="joke-list-item">{joke.text}<span></span></div>
+                
+              </section>
+            );
+          })}
+        </article></section>
+      </div>
+
       <br></br>
-      <div>
+
+      {/* <div>
         {allJokes.map((joke) => {
           return (
             <div key={joke.id}>
@@ -56,7 +119,11 @@ export const App = () => {
             </div>
           );
         })}
-      </div>
+      </div> */}
     </div>
+    </>
   );
 };
+
+
+//<button onClick={}>SAD BUTTON</button>
--- src/services/jokeService.js
+++ src/services/jokeService.js
@@ -1,17 +1,17 @@
 export const getAllJokes = () => {
-    return fetch (`http://localhost:8088/jokes`).then(res => res.json())
-}
+  return fetch(`http://localhost:8088/jokes`).then((res) => res.json());
+};
 
 export const saveJokeInJokeObject = () => {
-    fetch (`http://localhost:8088/jokes`)
-}
+  fetch(`http://localhost:8088/jokes`);
+};
 
 export const submitJoke = async (inputEvent) => {
-    const jokeObject = {
-      text: inputEvent,
-      told: false,
-    };
-    try {
+  const jokeObject = {
+    text: inputEvent,
+    told: false,
+  };
+  try {
     const response = await fetch("http://localhost:8088/jokes", {
       method: "POST",
       headers: {
@@ -20,13 +20,56 @@ export const submitJoke = async (inputEvent) => {
       body: JSON.stringify(jokeObject),
     });
     if (response.ok) {
-      console.log('Joke submitted successfully');
+      console.log("Joke submitted successfully");
       // You can reset the userInput state here if needed
-    
     } else {
-      console.error('Failed to submit joke');
+      console.error("Failed to submit joke");
     }
   } catch (error) {
-    console.error('Error submitting joke:', error);
+    console.error("Error submitting joke:", error);
   }
 };
+
+export const changeJokeValue = async (jokeObject) => {
+  const jokeData = {
+    key: jokeObject.id,
+    text: jokeObject.text,
+    told: !jokeObject.told,
+  };
+  console.log(jokeData);
+  const jokeOptions = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jokeData),
+  };
+
+  const response = await fetch(
+    `http://localhost:8088/jokes/${jokeObject.id}`,
+    jokeOptions
+  );
+  // const joke = await response.json()
+  // jokeObject = joke.told
+};
+//try{
+//   if (response.ok) {
+//     console.log('Joke updated successfully');
+//     // You can reset the userInput state here if needed
+
+//   } else {
+//     console.error('Failed to update joke');
+//   }
+// } catch (error) {
+//   console.error('Error updating joke:', error);
+// }
+
+// const element = document.querySelector('#put-request .date-updated');
+// const requestOptions = {
+//     method: 'PUT',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({ title: 'Fetch PUT Request Example' })
+// };
+// fetch('https://reqres.in/api/articles/1', requestOptions)
+//     .then(response => response.json())
+//     .then(data => element.innerHTML = data.updatedAt );
